7ca78cb0f1b558421bb7246a85d37c48e686940f
masihlapars-hub <masihlapars@gmail.com>
2026-05-26 18:17:36 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,57 @@
+const products = [
 
+{
+category:"Music",
+name:"Spotify Famplan",
+prices:[
+"1 Hari - 10K",
+"3 Hari - 12K",
+"5 Hari - 14K",
+"7 Hari - 17K",
+"1 Bulan - 25K",
+"2 Bulan - 60K",
+"3 Bulan - 80K"
+]
+},
+
+{
+category:"AI",
+name:"ChatGPT Private",
+prices:[
+"1 Hari - 13K",
+"3 Hari - 20K",
+"7 Hari - 27K",
+"1 Bulan - 135K"
+]
+},
+
+{
+category:"AI",
+name:"ChatGPT Sharing",
+prices:[
+"1 Hari - 9K",
+"3 Hari - 12K",
+"7 Hari - 17K",
+"1 Bulan - 42K"
+]
+},
+
+{
+category:"Design",
+name:"Canva Member",
+prices:[
+"1 Hari - 2K",
+"3 Hari - 4K",
+"7 Hari - 6K",
+"14 Hari - 8K",
+"1 Bulan - 10K",
+"2 Bulan - 12K",
+"3 Bulan - 15K",
+"4 Bulan - 18K",
+"5 Bulan - 22K",
+"6 Bulan - 25K",
+"1 Tahun - 32K"
+]
+}
+
+];
